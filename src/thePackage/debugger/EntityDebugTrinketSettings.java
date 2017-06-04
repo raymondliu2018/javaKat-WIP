207779ff471730753f3ba610a2cbd9619987cbb9
MrPentaholic <MrPentaholic@rayzPC.attlocal.net>
2017-06-04 08:41:24 -0700

    Still need more stuff

--- src/thePackage/debugger/EntityDebugTrinketSettings.java
+++ src/thePackage/debugger/EntityDebugTrinketSettings.java
@@ -8,8 +8,8 @@ public interface EntityDebugTrinketSettings {
     public static int ENTITY_COUNT_OFFSET_Y = 0;
     public static int ENTITY_TYPE_FONT_SIZE = 24;
     public static int ENTITY_COUNT_FONT_SIZE = 24;
-    public static int CONTROL_DEBUG_TRINKET_OFFSET_X = 0;
-    public static int CONTROL_DEBUG_TRINKET_OFFSET_Y = 100;
+    public static int CONTROL_DEBUG_TRINKET_OFFSET_X = 250;
+    public static int CONTROL_DEBUG_TRINKET_OFFSET_Y = 0;
     public static int RECT_DEBUG_TRINKET_OFFSET_X = 0;
     public static int RECT_DEBUG_TRINKET_OFFSET_Y = 0;
     public static String IMAGE = "thePackage/debugger/images/EntityDebugTrinket.png";
