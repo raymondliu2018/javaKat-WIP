a119e397addfdc831d646ded47756de60bc8fc24
MrPentaholic <MrPentaholic@rayzPC.attlocal.net>
2017-06-05 21:12:09 -0700

    Completed more of framework

--- src/thePackage/JukeBox.java
+++ src/thePackage/JukeBox.java
@@ -7,6 +7,7 @@ public final class JukeBox {
     private HashMap <String,Clip> sounds;
     private Entity owner;
     private String currentSound;
+    private Clip currentClip;
     public JukeBox(Entity input) {
         owner = input;
     }
@@ -16,14 +17,35 @@ public final class JukeBox {
     }
     
     public void addSound(String input, String name, boolean set){
-        
+        addSound(input,name);
+        if (set) {
+            setSound(name);
+        }
     }
     
     public void addSound(String input, String name) {
-        
+        //sounds.put(name, );
     }
     
     public String getCurrentSound() {
         return currentSound;
+    }
+    
+    public void setSound(String input){
+        currentSound = input;
+    }
+    
+    public void playSound(){
+        Clip clip = sounds.get(currentSound);
+        clip.setFramePosition(0);
+        clip.start();
+        currentClip = clip;
+    }
+    
+    public void interrupt() {
+        if (currentClip != null) {
+            currentClip.stop();
+        }
+        System.out.println("No clip to interrupt");
     }
 }
