7960223e8f69d80c258f2ae6065329b894b678a4
MrPentaholic <MrPentaholic@rayzPC.attlocal.net>
2017-06-10 09:21:58 -0700

    Everything should be stored in testing now

--- src/workspace/Script.java
+++ src/workspace/Script.java
@@ -1,44 +1,8 @@
 package workspace; 
 import thePackage.*;
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.event.KeyEvent;
-import thePackage.debugger.Debugger;
 
-public class Script implements SuperScript ,PlayerData {
-    static Player red,blue;
-    static String winner = null;
-    public static void init() { 
-        Debugger.enabled();
-        Camera.setBackground(Utility.scaleImage("images/Background.png",SCREEN_WIDTH,SCREEN_HEIGHT));
-        Speaker.setBackground("sounds/background.wav", 1.0f);
-        
-        red = new Player("red");
-        red.bindKeyToAction('w',"up");
-        red.bindKeyToAction('a',"left");
-        red.bindKeyToAction('s',"down");
-        red.bindKeyToAction('d',"right");
-        
-        red.bindKeyToAction(KeyEvent.VK_SPACE,"primary");
-        red.bindKeyToAction('q',"secondary");
-        
-        red.getRect().setCenterX(SCREEN_WIDTH/4.0);
-        red.getRect().setCenterY(SCREEN_HEIGHT/4.0);
-        
-        Manager.queueNewEntity(red);
-        
-        blue = new Player("blue");
-        blue.bindKeyToAction(KeyEvent.VK_UP,"up");
-        blue.bindKeyToAction(KeyEvent.VK_LEFT,"left");
-        blue.bindKeyToAction(KeyEvent.VK_DOWN,"down");
-        blue.bindKeyToAction(KeyEvent.VK_RIGHT,"right");
-        
-        blue.bindKeyToAction('/',"primary");
-        blue.bindKeyToAction('.',"secondary");
-        
-        blue.getRect().setCenterX(3.0*SCREEN_WIDTH/4.0);
-        blue.getRect().setCenterY(3.0*SCREEN_HEIGHT/4.0);
-        Manager.queueNewEntity(blue);
+public class Script implements SuperScript {
+    public static void init() {
     }
     
     public static void main(String [] args) {
@@ -49,26 +13,9 @@ public class Script implements SuperScript ,PlayerData {
     }
     
     public static void end() {
-        GameMaster.disable();
-        Text message = new Text();
-        message.setFont(new Font(Font.SANS_SERIF,Font.BOLD,48));
-        if (winner.equals("red")){
-            message.setColor(Color.RED);
-        }
-        if (winner.equals("blue")){
-            message.setColor(Color.BLUE);
-        }
-        message.setMessage(winner + " wins!");
-        message.setCenterX(GameMaster.getWidth()/2.0);
-        message.setCenterY(GameMaster.getHeight()/2.0);
-        Manager.addStat(message);
     }
     
     public static void collide(Entity entity1, Entity entity2) {
-    }
-    
-    public static void winner(String input) {
-        winner = input;
     }
 }
 
