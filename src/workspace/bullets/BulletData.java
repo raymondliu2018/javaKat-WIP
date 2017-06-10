d813497b3944150349153d6f149bbf1210c23989
MrPentaholic <MrPentaholic@rayzPC.attlocal.net>
2017-06-10 09:21:58 -0700

    Everything should be stored in testing now

--- src/workspace/bullets/BulletData.java
+++
@@ -1,8 +0,0 @@
-package workspace.bullets;
-
-public interface BulletData {
-    public static int BULLET_SIZE = 5;
-    public static int BULLET_SPEED = 5;
-    public static int BULLET_RANGE = 75;
-    public static int BOUNCY_BULLET_BOUNCE = 2;
-}
